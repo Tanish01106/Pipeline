02f53a49eb8ba6f918e025cde25e40c59c994366
Tanish01106 <ktitu0333@gmail.com>
2026-07-03 17:00:11 +0530

    Add Spark consumer: SparkSession, Kafka read, JSON parsing

---
+++ spark_consumer.py
@@ -0,0 +1,15 @@
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder \
+    .appName("PipelineConsumer") \
+    .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.13:3.5.0") \
+    .getOrCreate()
+
+df = spark.readStream \
+    .format("kafka") \
+    .option("kafka.bootstrap.servers", "100.77.136.82:9092") \
+    .option("subscribe", "login,order,payment") \
+    .option("startingOffsets", "earliest") \
+    .load()
+
+
